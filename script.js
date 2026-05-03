f3ee2180f0a47bb4ae6630fc91ad3f0a99e843cc
Slayniaccc <212771087+Slayniaccc@users.noreply.github.com>
2026-05-03 18:51:38 +0100

    Display full GitHub profile with bio, followers, repo count and profile link

--- script.js
+++ script.js
@@ -29,10 +29,15 @@ const profile = document.getElementById("profile");
 profile.innerHTML = 
 `<h1>${result.name}</h1>
 <img src="${result.avatar_url}">
- <p>${result.login}</p>`
+ <p>${result.login}</p>
+ <p>${result.bio}</p>
+ <p>${result.followers}</p>
+ <p>Public Repos: ${result.public_repos}</p>
+ <a href="${result.html_url}">View Github Profile</a>`
+
 
     console.log(result);
   } catch (err){
     console.log(err)
   }
-}
+  }
